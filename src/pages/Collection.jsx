4725ef879766c82037d4aa42db8b4074fa080c51
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-08-20 16:33:59 +0600

    category update

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import { useState } from 'react';
+import { assets } from '../assets/assets';
 
 const Collection = () => {
-  return <div></div>;
+  const [showFilter, setShowFilter] = useState(false);
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* filter options  */}
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+          <img
+            className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`}
+            src={assets.dropdown_icon}
+            alt=""
+          />
+        </p>
+
+        {/* category filter  */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? '' : 'hidden'
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">Categories</p>
+
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Men'} />
+              Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Women'} />
+              Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Kids'} />
+              Kids
+            </p>
+          </div>
+        </div>
+        {/* sub category Filter  */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? '' : 'hidden'
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">Type</p>
+
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Topwear'} />
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Bottomwear'} />
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3" type="checkbox" value={'Winterwear'} />
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
