c638b1fecbdae27ef2a5771fdfec16a938d2137c
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-08-24 23:04:29 +0600

    about and contact page upgrated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
-
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p className="text-sm text-gray-700 p-5">
+        Forever.com is a service oriented e-commerce business which gives you
+        the authority to unleash your shopaholic attitude from home with quality
+        products and world class customer support. At Forever.com, we strive to
+        utilize the power of the internet in its highest peak to fulfil the
+        needs of your busy life. We cover whole Bangladesh right now and still,
+        anyone from outside of Bangladesh can purchase products online providing
+        a Bangladeshi shipping address. Be comfy to experience the best shopping
+        experience from us.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
-
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="my-14 p-5">
+      <div className="text-sm font-medium text-gray-600">
+        <h2>Contact Us</h2>
+        <p>For any query please call us +023839393939</p>
+        <p>Or Send Us Email : support.forever@gmail.com</p>
+        <p>Office Location : North City,Dhaka-1200</p>
+      </div>
+      <div className="mt-10">
+        <h2>Please Complete the Form for Complain</h2>
+        <div className="mt-2">
+          <label>Your Name:</label>
+          <br />
+          <input
+            type="text"
+            className="px-2 py-2 mt-2 border sm:w-1/4 rounded-sm"
+            placeholder="Name"
+          />
+        </div>
+        <div className="mt-2">
+          <label>Email:</label>
+          <br />
+          <input
+            type="text"
+            className="px-2 py-2 mt-2 border sm:w-1/4 rounded-sm"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mt-2">
+          <label>Contact Number:</label>
+          <br />
+          <input
+            type="text"
+            className="px-2 py-2 mt-2 border sm:w-1/4  rounded-sm"
+            placeholder="Contact Number"
+          />
+        </div>
+
+        <textarea
+          className="border mt-2 h-24 sm:w-1/4 p-5"
+          name=""
+          id=""
+          placeholder="Write Your Objection"
+        ></textarea>
+      </div>
+      <button className="px-7 py-2 bg-black active:bg-gray-400 text-white mt-2 rounded-sm">
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
